78069ff95ab664b0ea553f068fb842b52fcc2fa6
Alexey <komkov08@gmail.com>
2023-06-20 15:00:17 +0300

    added hendlers start and help

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,12 @@
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from ..lexicon.lexicon import LEXIKON_RU
+
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXIKON_RU['/start'])
+
+@dp.message(Command(commands='/help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXIKON_RU['/help'])
+
